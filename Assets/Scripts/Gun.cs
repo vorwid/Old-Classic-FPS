a69746ac8c3cfbf9bd6a5741259e86b3fbd98e87
Łukasz <lukasz.m.koscielniak@gmail.com>
2021-01-20 12:55:14 +0100

    Gun reload and sound scripts

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
@@ -9,19 +10,51 @@ public class Gun : MonoBehaviour
     public float GunDamage;
     public float GunRange;
     public AudioClip ShotSound;
+    public AudioClip ReloadSound;
+    public AudioClip EmptyGunSound;
+
+    public Text AmmoText;
+
+    public int AmmoAmount;
+    public int AmmoClipSize;
+    int AmmoLeft;
+    int AmmoClipLeft;
+
+    bool isShot;
+    bool isReloading;
+
     AudioSource source;
 
     private void Awake()
     {
+
         source = GetComponent<AudioSource>();
+        AmmoLeft = AmmoAmount;
+        AmmoClipLeft = AmmoClipSize;
     }
+
+    private void Update()
+    {
+        AmmoText.text = AmmoClipLeft + " / " + AmmoLeft;
+
+        if (Input.GetButtonDown("Fire1"))
+            isShot = true;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
 
     private void FixedUpdate()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Input.GetButtonDown("Fire1"))
+        if (isShot == true && AmmoClipLeft > 0 && isReloading == false)
         {
+            isShot = false;
+            AmmoClipLeft--;
             source.PlayOneShot(ShotSound);
             StartCoroutine("shot");  //wywolujemy korutyne
             if(Physics.Raycast(ray, out hit, GunRange))
@@ -30,6 +63,40 @@ public class Gun : MonoBehaviour
                 hit.collider.gameObject.SendMessage("GunHit", GunDamage, SendMessageOptions.DontRequireReceiver);                 //zdalne wywolanie funkcji w tym obiekcie
             }
         }
+        else if (isShot == true && AmmoClipLeft <= 0 && isReloading == false)
+        {
+            isShot = false;
+            Reload();
+        }
+    }
+
+    void Reload()
+    {
+        int BulletstoReload = AmmoClipSize - AmmoClipLeft;
+        if(AmmoLeft >= BulletstoReload)
+        {
+            StartCoroutine("ReloadWeapon");
+            AmmoLeft -= BulletstoReload;
+            AmmoClipLeft = AmmoClipSize;
+        }
+        else if (AmmoLeft < BulletstoReload && AmmoLeft > 0)
+        {
+            StartCoroutine("ReloadWeapon");
+            AmmoClipLeft += AmmoLeft;
+            AmmoLeft = 0;
+        }
+        else if (AmmoLeft <= 0)
+        {
+            source.PlayOneShot(EmptyGunSound);
+        }
+    }
+
+    IEnumerator ReloadWeapon()
+    {
+        isReloading = true;
+        source.PlayOneShot(ReloadSound);
+        yield return new WaitForSeconds(2);
+        isReloading = false;
     }
 
     IEnumerator shot()
